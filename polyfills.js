b3c4df0ce47f70740c23fbec599619b077a8cc45
Igal <igal.fl@gmail.com>
2014-01-09 16:19:38 -0800

    Add dynamicSort underscore extension

--- polyfills.js
+++ polyfills.js
@@ -31,6 +31,19 @@ _.mixin({
       throw new Meteor.Error(err ? err : "oops, assert failed");
     }
   },
+
+  dynamicSort: function (property) {
+    var sortOrder = 1;
+    if(property[0] === "-") {
+      sortOrder = -1;
+      property = property.substr(1);
+    }
+    return function (a,b) {
+      var result = (a[property] < b[property]) ? 
+        -1 : (a[property] > b[property]) ? 1 : 0;
+      return result * sortOrder;
+    }
+  },
 });
 
 }());
