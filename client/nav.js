b8c0fef1d11b9f09a37e6e2ab801f50f73ba988b
Igal <igal.fl@gmail.com>
2014-02-14 14:44:30 -0800

    Add fullscreen support

--- client/nav.js
+++ client/nav.js
@@ -14,4 +14,46 @@ Template.header.getFbUser = function () {
   //console.log("getFbUser", user);
   var s = user.services;
   return s && s.facebook ? s.facebook.username : undefined;
-}
+};
+
+Template.header.isFullScreen = function () {
+  // return Session.get("isFullScreen");
+  // or - directly query the window state:
+  return document.fullscreenElement ||    // alternative standard method
+      document.mozFullScreenElement ||
+      document.webkitFullscreenElement ||
+      document.msFullscreenElement; // current working methods
+};
+
+function enterFullScreen() {
+  if (document.documentElement.requestFullscreen) {
+    document.documentElement.requestFullscreen();
+  } else if (document.documentElement.msRequestFullscreen) {
+    document.documentElement.msRequestFullscreen();
+  } else if (document.documentElement.mozRequestFullScreen) {
+    document.documentElement.mozRequestFullScreen();
+  } else if (document.documentElement.webkitRequestFullscreen) {
+    document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+  }
+};
+
+function exitFullScreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+};
+
+Template.header.events({
+  'click #fullscreen-on': function () {
+    enterFullScreen();
+  },
+  'click #fullscreen-off': function () {
+    exitFullScreen();
+  },
+});
