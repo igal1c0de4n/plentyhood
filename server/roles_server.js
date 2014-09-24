53be6856f3ba01777346c8f91836c68739b84ce0
Igal <igal.fl@gmail.com>
2014-09-23 19:25:24 -0700

    Whitespace: sublime text beautify

--- server/roles_server.js
+++ server/roles_server.js
@@ -1,66 +1,74 @@
-;(function () {
-
+(function() {
   "use strict";
-
-////////////////////////////////////////////////////////////////////
-// Patches
-
-if (!console || !console.log) {
-  // stub for IE
-  console = { 
-    log: function (msg) {
-      $('#log').append(msg)
-    } 
-  };
-}
-
-////////////////////////////////////////////////////////////////////
-// Startup
-
-Meteor.startup(function () {
-  // Create Demo Users
-  if (Meteor.users.find().fetch().length === 0) {
-    console.log('Creating users: ');
-    var users = [
-      {name:"user", email:"user@ph.earth", roles: []},
-      {name:"admin", email:"admin@ph.earth", roles: ['admin']}
-    ];
-    _.each(users, function (userData) {
-      var id,
-      user;
-      console.log(userData);
-      id = Accounts.createUser({
-        email: userData.email,
-        password: "sharewme",
-        profile: { name: userData.name }
-      });
-      // email verification
-      Meteor.users.update({_id: id}, {$set:{'emails.0.verified': true}});
-      Roles.addUsersToRoles(id, userData.roles);
-    });
+  if (!console || !console.log) {
+    // stub for IE
+    console = {
+      log: function(msg) {
+        $('#log').append(msg)
+      }
+    };
   }
-  Accounts.onCreateUser(function (options, user) {
-    console.log("onCreateUser options", options);
-    console.log("onCreateUser user", user);
-    //     user.services = options.services;
-    if (options.profile) {
-      // store for profile picture etc
-      user.profile = options.profile;
+  Meteor.startup(function() {
+    // Create Demo Users
+    if (Meteor.users.find().fetch().length === 0) {
+      console.log('Creating users: ');
+      var users = [{
+        name: "user",
+        email: "user@ph.earth",
+        roles: []
+      }, {
+        name: "admin",
+        email: "admin@ph.earth",
+        roles: ['admin']
+      }];
+      _.each(users, function(userData) {
+        var id,
+          user;
+        console.log(userData);
+        id = Accounts.createUser({
+          email: userData.email,
+          password: "sharewme",
+          profile: {
+            name: userData.name
+          }
+        });
+        // email verification
+        Meteor.users.update({
+          _id: id
+        }, {
+          $set: {
+            'emails.0.verified': true
+          }
+        });
+        Roles.addUsersToRoles(id, userData.roles);
+      });
     }
-    return user;
+    Accounts.onCreateUser(function(options, user) {
+      console.log("onCreateUser options", options);
+      console.log("onCreateUser user", user);
+      //     user.services = options.services;
+      if (options.profile) {
+        // store for profile picture etc
+        user.profile = options.profile;
+      }
+      return user;
+    });
   });
-});
 
-////////////////////////////////////////////////////////////////////
-// Publish
-
-// Authorized users can manage user accounts
-Meteor.publish("users", function () {
-  var user = Meteor.users.findOne({_id:this.userId});
-  if (Roles.userIsInRole(user, ["admin"])) {
-    return Meteor.users.find({}, {fields: {emails: 1, profile: 1, roles: 1}});
-  } 
-  this.stop();
-});
-
+  // Authorized users can manage user accounts
+  Meteor.publish("users", function() {
+    var user = Meteor.users.findOne({
+      _id: this.userId
+    });
+    if (Roles.userIsInRole(user, ["admin"])) {
+      return Meteor.users.find({}, {
+        fields: {
+          emails: 1,
+          profile: 1,
+          roles: 1
+        }
+      });
+    }
+    this.stop();
+  });
 }());
--- server/server.js
+++ server/server.js
@@ -1,55 +1,94 @@
-;(function () {
+(function() {
   "use strict";
 
-Meteor.publish("directory", function () {
-  return Meteor.users.find({}, {fields: {emails: 1, profile: 1}});
-});
-
-Meteor.publish("userDetails", function () {
-  return Meteor.users.find(
-    {_id: this.userId},
-    {fields: {profile: 1, emails: 1, services: 1}});
-});
-
-Meteor.publish("places", function (bounds) {
-  if (bounds) {
-    var cursor = collections.Places.find({
-      location: {$geoWithin : {$box: bounds}},
-      $or: [{"public": true}, {invited: this.userId}, {owner: this.userId}]
-    });
-    // console.log("publishing", cursor.fetch().length, "places", bounds);
-    return cursor;
-  }
-});
-
-Meteor.publish("tags", function () {
-  return collections.Tags.find();
-});
-
-Meteor.publish("resources", function () {
-  return collections.Resources.find();
-});
-
-collections.Places._ensureIndex({location : "2dsphere"});
-
-//------------ db migration code -----------
-
-var dataMassage = function () {
-  _.each(collections.Places.find().fetch(), function (p) {
-    _.each(p.resources, function (rid) {
-      var r = collections.Resources.findOne(rid);
-      if (!r.placeId) {
-        console.log("set placeId", p._id, "in resource", rid);
-        var ret = collections.Resources.update(rid, {$set: {placeId: p._id}});
-      } else {
-        console.log("placeId", p._id, "already in resource", rid);
+  Meteor.publish("directory", function() {
+    return Meteor.users.find({}, {
+      fields: {
+        emails: 1,
+        profile: 1
       }
     });
   });
-  var ret = collections.Resources.update(
-    {place: {$exists: true}}, {$unset: {place: 1}}, {multi: true});
-  var ret = collections.Resources.remove({placeId: {$exists: false}});
-};
-// dataMassage();
 
+  Meteor.publish("userDetails", function() {
+    return Meteor.users.find({
+      _id: this.userId
+    }, {
+      fields: {
+        profile: 1,
+        emails: 1,
+        services: 1
+      }
+    });
+  });
+
+  Meteor.publish("places", function(bounds) {
+    if (bounds) {
+      var cursor = collections.Places.find({
+        location: {
+          $geoWithin: {
+            $box: bounds
+          }
+        },
+        $or: [{
+          "public": true
+        }, {
+          invited: this.userId
+        }, {
+          owner: this.userId
+        }]
+      });
+      // console.log("publishing", cursor.fetch().length, "places", bounds);
+      return cursor;
+    }
+  });
+
+  Meteor.publish("tags", function() {
+    return collections.Tags.find();
+  });
+
+  Meteor.publish("resources", function() {
+    return collections.Resources.find();
+  });
+
+  collections.Places._ensureIndex({
+    location: "2dsphere"
+  });
+
+  //------------ db migration code -----------
+
+  var dataMassage = function() {
+    _.each(collections.Places.find().fetch(), function(p) {
+      _.each(p.resources, function(rid) {
+        var r = collections.Resources.findOne(rid);
+        if (!r.placeId) {
+          console.log("set placeId", p._id, "in resource", rid);
+          var ret = collections.Resources.update(rid, {
+            $set: {
+              placeId: p._id
+            }
+          });
+        } else {
+          console.log("placeId", p._id, "already in resource", rid);
+        }
+      });
+    });
+    var ret = collections.Resources.update({
+      place: {
+        $exists: true
+      }
+    }, {
+      $unset: {
+        place: 1
+      }
+    }, {
+      multi: true
+    });
+    var ret = collections.Resources.remove({
+      placeId: {
+        $exists: false
+      }
+    });
+  };
+  // dataMassage();
 }());
